bc9bfd344d727f6d630fb046d2bb3d0c5517a64a
unknown <mateopacheco2002s@gmail.com>
2022-09-15 19:58:56 -0300

    JSprincipio

---
+++ clase13/JS/operaciones.js
@@ -0,0 +1,9 @@
+function Saludar () {
+    alert('hola mundo dentro de saludar ()');
+}
+
+let op1 = prompt('ingrese op1');
+let op2 = prompt('ingrese op2')
+let res = (+op1) + parseInt(op2)
+console.log (res);
+//alert(res);
