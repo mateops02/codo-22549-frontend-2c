02e5aa65c82cbaca0ea6e60aa6c061e76e3317bf
unknown <mateopacheco2002s@gmail.com>
2022-09-29 18:01:25 -0300

    clase 16

--- clase 16/java/ejercicio.js
+++ clase 16/java/ejercicio.js
@@ -1,20 +1,44 @@
-function calcularNotas () {
-    const nota1 = document.getElementById("nota1").value
-    const nota2 = document.getElementById("nota2").value
+//creamos funcion anashei
+function calcularnotas () {
+    const nota1 = document.getElementById ("nota1").value;
+    const nota2     = document.getElementById ("nota2").value;
 
-    const notas = []
-
-    notas[0] = parseInt(nota1);
+    //creamos el vector de notas god
+    const notas = []; 
+    notas[0] = +nota1;
     notas[1] = +nota2;
 
-    //ahora laburo con el vector
-    //recorro el vector
+    //ahora a laburar al vector
+    //recorremos vector con ciclo for
 
-    let menor = [0];
-    let mayor = [0];
-    let promedio = [0]
+    let minimo = notas[0];
+    let maximo = notas[0];
+    let promedio;
+    let suma = 0;
 
-    for(let i=0;i<notas.length;i++) {
-        console.log(notas[i]);
+    for(let i=0 ; i<notas.length ; i++ ) {
+
+        if (notas[i] < minimo ) {
+            minimo = notas[i];
+        }
+
+        if (notas[i] > maximo ) {
+            maximo = notas[i];
+        }
+        //sumamos las notaas
+        suma = suma + notas[i];
     }
+    //calculemos el promedio anashei con las sumas
+    promedio = suma / notas.length;
+
+    console.log(maximo);
+    console.log(minimo);
+    console.log(promedio);
+
+    document.getElementById("menor").value = minimo
+    document.getElementById("mayor").value = maximo
+    document.getElementById("promedio").value = promedio
+
+   
+
 }
